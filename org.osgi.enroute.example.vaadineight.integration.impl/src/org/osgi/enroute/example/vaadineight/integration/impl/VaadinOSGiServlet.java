1c33c7aed706d2c87445a427d6d7d38f6a845fc1
Sandared <thomas.driessen.td@gmail.com>
2018-01-18 14:46:03 +0100

    Handle AppWidgetset

--- org.osgi.enroute.example.vaadineight.integration.impl/src/org/osgi/enroute/example/vaadineight/integration/impl/VaadinOSGiServlet.java
+++ org.osgi.enroute.example.vaadineight.integration.impl/src/org/osgi/enroute/example/vaadineight/integration/impl/VaadinOSGiServlet.java
@@ -57,6 +57,13 @@ public class VaadinOSGiServlet extends VaadinServlet {
 			service.setClassLoader(new ClassLoader() {
 				@Override
 				public Class<?> loadClass(String name) throws ClassNotFoundException {
+					// The Vaadin BootstrapHandler tries to get a WidgetsetInfo in its setupMainDiv method, 
+					// which calls the getWidgetsetInfo method of UIProvider,
+					// which in turn tries to load the class "AppWidgetset". 
+					// According to the comments in UIProvider.findWidgetsetClass(), a ClassNotFoundException is a normal case, 
+					// so I will just throw this Exception in my custom ClassLoader whenever it is asked for this class.
+					if("AppWidgetset".equals(name))
+						throw new ClassNotFoundException("This is an allowed return value for AppWidgetset");
 					try {
 						try {
 							Class<?> loadClass = VaadinOSGiServlet.class.getClassLoader().loadClass(name);
